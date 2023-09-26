a01c1c266fa0bcb89dd69cf2915a4ee1a33bb0c5
Nhant <nhantk27023@gmail.com>
2023-09-26 10:07:49 +0700

    add code

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -2,15 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(this, signup.class ));
     }
 }
